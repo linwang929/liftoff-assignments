4a747d1db0d62fdb93711ea34bb7ca352fb00688
linwang929 <lin.wang@go.wustl.edu>
2018-08-06 22:54:21 -0500

    created initial project

---
+++ mentor-hub/src/main/java/com/example/mentorhub/MentorHubApplication.java
@@ -0,0 +1,12 @@
+package com.example.mentorhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorHubApplication.class, args);
+	}
+}
---
+++ mentor-hub/src/test/java/com/example/mentorhub/MentorHubApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.mentorhub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MentorHubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
